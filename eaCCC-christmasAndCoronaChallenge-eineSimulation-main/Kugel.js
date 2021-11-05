a7cafbf5f08fda31b85b56534e10845914a4962b
Menersar <menersar@menersar.com>
2021-11-05 02:16:20 +0100

    Update Kugel.js

--- eaCCC-christmasAndCoronaChallenge-eineSimulation-main/Kugel.js
+++ eaCCC-christmasAndCoronaChallenge-eineSimulation-main/Kugel.js
@@ -1,24 +1,42 @@
 
 
 
+
 class Kugel {
 
 
-	constructor(_id, _radius, _gesund, _minPosition, _maxPosition, _zeitZumGesundwerden) {
+	constructor(_id, _radius, _gesund, _minPosition, _maxPosition, _zeitZumGesundwerden, _kugelModels) {
 
 
 
 		this.id = _id;
-		this.startPunkt = [getRandomArbitrary(_minPosition, _maxPosition), getRandomArbitrary(_minPosition, _maxPosition), getRandomArbitrary(_minPosition, _maxPosition)];
-		this.richtung = [randomIntFromInterval(0, 1), randomIntFromInterval(0, 1), randomIntFromInterval(0, 1)];
-		this.geschwindigkeit = getRandomArbitrary(1, 10) * .001;
 		this.radius = _radius;
+		this.startPunkt; 
+		this.createPosition(_minPosition, _maxPosition, _kugelModels);
+		this.richtung = [getRandomArbitrary(0.1, 1), getRandomArbitrary(0.1, 1), getRandomArbitrary(0.1, 1)];
+		//console.log("before:"+this.richtung[0]+"|"+this.richtung[1]+"|"+this.richtung[2]);
+		vec3.normalize(this.richtung,this.richtung);
+		//console.log("after:"+this.richtung[0]+"|"+this.richtung[1]+"|"+this.richtung[2]);
+		this.geschwindigkeit = getRandomArbitrary(1, 10) * .001;
 		this.gesund = _gesund;
 		this.vergangeneZeitschritte = 0;
 		this.zeitZumGesundwerden = _zeitZumGesundwerden;
 
 		this.immun = false;
 
+	}
+
+
+	createPosition (_minPos, _maxPos, _kugelMod) {
+		this.startPunkt =  [getRandomArbitrary(_minPos, _maxPos), getRandomArbitrary(_minPos, _maxPos), getRandomArbitrary(_minPos, _maxPos)];
+
+		if (_kugelMod.length > 0) {
+			_kugelMod.forEach((k) => {
+			if (this.intersect(k.startPunkt)) {
+				this.createPosition(_minPos, _maxPos, _kugelMod);
+			}
+		});
+	}
 	}
 
 
@@ -44,9 +62,8 @@ class Kugel {
 		}
 
 
-		this.startPunkt[0] = this.startPunkt[0] + this.richtung[0] * this.geschwindigkeit;
-		this.startPunkt[1] = this.startPunkt[1] + this.richtung[1] * this.geschwindigkeit;
-		this.startPunkt[2] = this.startPunkt[2] + this.richtung[2] * this.geschwindigkeit;
+		vec3.scaleAndAdd(this.startPunkt, this.startPunkt, this.richtung, this.geschwindigkeit);
+
 
 		if (this.startPunkt[0] + this.radius > 1) {
 			this.startPunkt[0] = -1 + this.radius;
@@ -84,13 +101,24 @@ class Kugel {
 
 	testInfection(otherKugeln) {
 		//console.log("test infection");
-		if (!this.gesund) {
+	/*	if (!this.gesund) {
 			return;
-		}
-		if (this.gesund) {
+		}*/
+	//	if (this.gesund) {
 			//let ges = true;
 			otherKugeln.forEach((k) => {
 				if (k.id != this.id && this.intersect(k.startPunkt)) {
+					
+					vec3.normalize(k.richtung, vec3.subtract(this.richtung, this.richtung, k.richtung));
+					vec3.negate(this.richtung, k.richtung);
+
+					vec3.scaleAndAdd(this.startPunkt, this.startPunkt, this.richtung, this.radius*.1);
+					vec3.scaleAndAdd(k.startPunkt, k.startPunkt, k.richtung, k.radius*.1);
+
+
+					//vec3.negate(this.richtung, this.richtung);
+					//vec3.negate(k.richtung, k.richtung);
+				//	vec3.normalize(this.richtung,this.richtung);
 				//	console.log("test intersect");
 					if (!k.gesund) {
 					//	console.log("he touched me");
@@ -99,7 +127,9 @@ class Kugel {
 				}
 			});
 
-		}
+	//	} 
+					
+				
 	}
 
 
@@ -107,12 +137,10 @@ class Kugel {
 
 	intersect(otherKugelPosition) {
 		let r = this.radius;
-		let d = Math.sqrt((otherKugelPosition[0] - this.startPunkt[0]) * (otherKugelPosition[0] - this.startPunkt[0])
-			+ (otherKugelPosition[1] - this.startPunkt[1]) * (otherKugelPosition[1] - this.startPunkt[1])
-			+ (otherKugelPosition[2] - this.startPunkt[2]) * (otherKugelPosition[2] - this.startPunkt[2]));
+		let d =	vec3.distance(otherKugelPosition, this.startPunkt);
 		//console.log("radius " + radius+radius);
 
-		return d < (r + r);
+		return d <= (r + r);
 	}
 
 
@@ -138,6 +166,19 @@ function getRandomArbitrary(min, max) {
 function randomIntFromInterval(min, max) { // min and max included 
 	return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+/*
+
+function magnitude (x,y,z) {
+    return Math.sqrt(x * x + y * y + z + z);
+}
+
+
+
+function normalizeVector (v) {
+    var mag = this.magnitude(v[0],v[1],v[2]);
+    return new Vector(this.mX/mag, this.mY/mag, this.mZ/mag);
+};*/
 
 /*
 
